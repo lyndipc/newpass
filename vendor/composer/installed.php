ac1c690eeeb43a10ff0fb7c340acea7a047280f2
Lyndi Castrejon <lyndic@protonmail.com>
2023-08-01 18:54:51 -0600

    Add composer files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'lyndipc/newpass',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'af828986bcc4a5a37b2b28fd3cc7e464cc422b31',
+        'type' => 'clilibrary',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'lyndipc/newpass' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'af828986bcc4a5a37b2b28fd3cc7e464cc422b31',
+            'type' => 'clilibrary',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
